1d3f8f5dd566ce517bddcc58901de040cbd045ed
cristi <scarlat.cristian.gabriel@gmail.com>
2023-11-05 00:07:37 +0200

    optimised

--- main.py
+++ main.py
@@ -15,9 +15,18 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(
         title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name?")
 
+    if answer_state == 'exit' or answer_state == None:
+        missing_states = []
+        for state in data["state"]:
+            if data["state"][state] not in guessed_states:
+                missing_states.append(data["state"][state])
+        new_data = pandas.DataFrame(
+            missing_states).to_csv("states_to_learn.csv")
+        break
+
     for state in data["state"]:
-        if data["state"][state].lower().replace(" ", "") == answer_state.lower().replace(" ", ""):
-            guessed_states.append(answer_state)
+        if data["state"][state] == answer_state.title():
+            guessed_states.append(answer_state.title())
             state_turtle = turtle.Turtle()
             state_turtle.hideturtle()
             state_turtle.penup()
@@ -30,6 +39,3 @@ while len(guessed_states) < 50:
 # def get_mouse_click_coors(x, y):
 #     print(x, y)
 # turtle.onscreenclick(get_mouse_click_coors)
-
-
-turtle.mainloop()
